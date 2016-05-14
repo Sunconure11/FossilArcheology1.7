5ce25a6ee5c5870db9f9d5cb36c4c52b7266ac3e
Absinthe <Sunconure11@users.noreply.github.com>
2016-05-14 19:15:19 -0400

    Update FossilOreDictionary.java

--- src/main/java/com/github/revival/server/handler/FossilOreDictionary.java
+++ src/main/java/com/github/revival/server/handler/FossilOreDictionary.java
@@ -49,6 +49,7 @@ public class FossilOreDictionary {
 		OreDictionary.registerOre("materialRotten", new ItemStack(FAItemRegistry.INSTANCE.failuresaurusFlesh));
 		OreDictionary.registerOre("slimeball", new ItemStack(FAItemRegistry.INSTANCE.failuresaurusFlesh));
 		OreDictionary.registerOre("tar", new ItemStack(FAItemRegistry.INSTANCE.tardrop));
+		OreDictionary.registerOre("tarBucket", new ItemStack(FAItemRegistry.INSTANCE.tar_bucket));
 
 		OreDictionary.registerOre("dinosaurSkull", new ItemStack(FAItemRegistry.INSTANCE.skull, 1, OreDictionary.WILDCARD_VALUE));
 		OreDictionary.registerOre("dinosaurLegBone", new ItemStack(FAItemRegistry.INSTANCE.legBone, 1, OreDictionary.WILDCARD_VALUE));
