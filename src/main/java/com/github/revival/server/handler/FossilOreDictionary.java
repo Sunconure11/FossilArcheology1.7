0397e3fa39719e8bcb3bec9a0d75d113ea61ee1b
Absinthe <Sunconure11@users.noreply.github.com>
2016-05-14 20:16:30 -0400

    Update FossilOreDictionary.java

--- src/main/java/com/github/revival/server/handler/FossilOreDictionary.java
+++ src/main/java/com/github/revival/server/handler/FossilOreDictionary.java
@@ -38,6 +38,7 @@ public class FossilOreDictionary {
 		OreDictionary.registerOre("skullLantern", new ItemStack(FABlockRegistry.INSTANCE.skullLantern));
 		OreDictionary.registerOre("chest", new ItemStack(FABlockRegistry.INSTANCE.ancientChest));
 		OreDictionary.registerOre("stoneAncient", new ItemStack(FABlockRegistry.INSTANCE.ancientStone));
+		OreDictionary.registerOre("stoneAncientBrick", new ItemStack(FABlockRegistry.INSTANCE.ancientStonebrick));
 
 		OreDictionary.registerOre("artifact", new ItemStack(FAItemRegistry.INSTANCE.relic));
 		OreDictionary.registerOre("fossil", new ItemStack(FAItemRegistry.INSTANCE.biofossil));
