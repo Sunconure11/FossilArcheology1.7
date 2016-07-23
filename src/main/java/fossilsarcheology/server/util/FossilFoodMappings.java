9c5d43fa72e5fbc556747907f91e81a6f60bd939
Absinthe <Sunconure11@users.noreply.github.com>
2016-07-23 08:37:24 -0400

    Update FossilFoodMappings.java

--- src/main/java/fossilsarcheology/server/util/FossilFoodMappings.java
+++ src/main/java/fossilsarcheology/server/util/FossilFoodMappings.java
@@ -164,7 +164,9 @@ public class FossilFoodMappings {
         FoodMappings.INSTANCE.addPlant("treeLeaves", 20);
         FoodMappings.INSTANCE.addPlant("treeSapling", 15);
         FoodMappings.INSTANCE.addPlant("sapling", 15);
-        FoodMappings.INSTANCE.addPlant("saplingPalm", 15);        
+        FoodMappings.INSTANCE.addPlant("saplingPalm", 15);   
+        FoodMappings.INSTANCE.addPlant("leaves", 20);
+        FoodMappings.INSTANCE.addPlant("leavesPalm", 20);
         FoodMappings.INSTANCE.addPlant("listAllgrain", 10);
         FoodMappings.INSTANCE.addPlant("cropRice", 10);
         FoodMappings.INSTANCE.addPlant("cropCorn", 10);
